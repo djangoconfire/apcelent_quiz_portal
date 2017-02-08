a7a396e9263e81388ac28d3e302af6a5762440cd
djangoconfire <djangopycon@gmail.com>
2017-02-08 13:02:28 +0530

    added static urls

--- quiz_system/urls.py
+++ quiz_system/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     
@@ -11,4 +12,9 @@ urlpatterns = [
 
     # for api
     url(r'^api/', include('api.urls',namespace="api")),   
+]
+
+# for staticfiles
+urlpatterns +=[
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
